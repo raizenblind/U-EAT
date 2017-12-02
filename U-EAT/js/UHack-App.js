495361d72d86b7645f108299f1ebb91f93edc771
Dev09 <Dev09@LAPTOP-OJBTBCT8>
2017-12-03 04:44:44 +0800

    Added Map Function

--- U-EAT/js/UHack-App.js
+++ U-EAT/js/UHack-App.js
@@ -1,10 +1,20 @@
 
 
-$(document).ready(function() {
+$(document).ready(function () {
     $(".button-collapse").sideNav();
     $('.modal').modal();
-    $('.tooltipped').tooltip({delay: 50});
+    $('.tooltipped').tooltip({ delay: 50 });
+
 });
 
-
+$('.carousel.carousel-slider').carousel({ fullWidth: true });
 $('#modal1').modal('open');
+
+function myMap() {
+    var myCenter = new google.maps.LatLng(51.508742, -0.120850);
+    var mapCanvas = document.getElementById("map");
+    var mapOptions = { center: myCenter, zoom: 5 };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({ position: myCenter });
+    marker.setMap(map);
+}
